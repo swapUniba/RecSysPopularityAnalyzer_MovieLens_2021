053d064da8d803ef725edd8be2f71f03c813f3ad
Antonio Valletta <61507922+Antoniovalletta7@users.noreply.github.com>
2021-10-11 15:17:24 +0200

    Aggiunta modulo Convertion

--- Convertion/convertion.py
+++ Convertion/convertion.py
@@ -1,1 +1,8 @@
+import fastparquet
+
+pfile = fastparquet.ParquetFile('../recs/cf/recommendations.parquet')
+with open('recommendations.csv', 'w') as fp:
+    for i, df in enumerate(pfile.iter_row_groups()):
+        write_header = (i==0)
+        df.to_csv(fp, index=False, header=write_header)
 
